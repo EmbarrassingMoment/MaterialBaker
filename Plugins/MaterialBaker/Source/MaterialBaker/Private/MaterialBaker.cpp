a8969b42307b584e86a1465fdea8e793d4d2cd00
EmbarrassingMoment <96903403+EmbarrassingMoment@users.noreply.github.com>
2025-06-17 22:11:47 +0900

    Fix code error

--- Plugins/MaterialBaker/Source/MaterialBaker/Private/MaterialBaker.cpp
+++ Plugins/MaterialBaker/Source/MaterialBaker/Private/MaterialBaker.cpp
@@ -56,70 +56,69 @@ void FMaterialBakerModule::ShutdownModule()
 
 TSharedRef<SDockTab> FMaterialBakerModule::OnSpawnPluginTab(const FSpawnTabArgs& SpawnTabArgs)
 {
-                + SVerticalBox::Slot()
-                .AutoHeight()
-                .Padding(5.0f)
-                [
-                    SNew(SObjectPropertyEntryBox)
-                        .AllowedClass(UMaterialInterface::StaticClass())
-                        .ObjectPath_Lambda([this]() -> FString { return SelectedMaterial ? SelectedMaterial->GetPathName() : FString(); })
-                        .OnObjectChanged_Lambda([this](const FAssetData& AssetData) { SelectedMaterial = Cast<UMaterialInterface>(AssetData.GetAsset()); })
-                        .MinDesiredWidth(300.f)
-                ]
+    TSharedRef<SVerticalBox> WidgetContent = SNew(SVerticalBox)
+        + SVerticalBox::Slot()
+        .AutoHeight()
+        .Padding(5.0f)
+        [
+            SNew(SBox)
+            .WidthOverride(300.f)
+            [
+                SNew(SObjectPropertyEntryBox)
+                .AllowedClass(UMaterialInterface::StaticClass())
+                .ObjectPath_Lambda([this]() -> FString { return SelectedMaterial ? SelectedMaterial->GetPathName() : FString(); })
+                .OnObjectChanged_Lambda([this](const FAssetData& AssetData) { SelectedMaterial = Cast<UMaterialInterface>(AssetData.GetAsset()); })
+            ]
+        ]
 
-		// マテリアルを選択するアセットピッカー（後で実装）
-		+ SVerticalBox::Slot()
-		.AutoHeight()
-		.Padding(5.0f)
-		[
-			// 今はプレースホルダーとしてSBoxを配置
-			SNew(SBox)
-				.MinDesiredWidth(300.f)
-				[
-					SNew(STextBlock)
-						.Text(LOCTEXT("MaterialAssetPicker", " Material Asset Picker will go here ")) // "Material Asset Picker will go here"
-				]
-		]
+        // マテリアルを選択するアセットピッカー（後で実装）
+        + SVerticalBox::Slot()
+        .AutoHeight()
+        .Padding(5.0f)
+        [
+            SNew(SBox)
+            .MinDesiredWidth(300.f)
+            [
+                SNew(STextBlock)
+                .Text(LOCTEXT("MaterialAssetPicker", " Material Asset Picker will go here "))
+            ]
+        ]
 
-	// テクスチャサイズのラベル
-	+ SVerticalBox::Slot()
-		.AutoHeight()
-		.Padding(5.0f)
-		[
-			SNew(STextBlock)
-				.Text(LOCTEXT("TextureSizeLabel", "Bake Texture Size"))
-		]
+        // テクスチャサイズのラベル
+        + SVerticalBox::Slot()
+        .AutoHeight()
+        .Padding(5.0f)
+        [
+            SNew(STextBlock)
+            .Text(LOCTEXT("TextureSizeLabel", "Bake Texture Size"))
+        ]
 
-		// テクスチャサイズを選択するドロップダウン（後で実装）
-		+ SVerticalBox::Slot()
-		.AutoHeight()
-		.Padding(5.0f)
-		[
-			// 今はプレースホルダーとしてSBoxを配置
-			SNew(SBox)
-				[
-					SNew(STextBlock)
-						.Text(LOCTEXT("TextureSizeDropdown", " Texture Size Dropdown will go here ")) // "Texture Size Dropdown will go here"
-				]
-		]
+        // テクスチャサイズを選択するドロップダウン（後で実装）
+        + SVerticalBox::Slot()
+        .AutoHeight()
+        .Padding(5.0f)
+        [
+            SNew(SBox)
+            [
+                SNew(STextBlock)
+                .Text(LOCTEXT("TextureSizeDropdown", " Texture Size Dropdown will go here "))
+            ]
+        ]
 
-	// ベイク実行ボタン
-	+ SVerticalBox::Slot()
-		.HAlign(HAlign_Right) // 右寄せ
-		.Padding(10.0f)
-		[
-			SNew(SButton)
-				.Text(LOCTEXT("BakeButton", "Bake Material"))
-				/* .OnClicked( ... ) */ // ボタンが押された時の処理を後で追加
-		];
+        // ベイク実行ボタン
+        + SVerticalBox::Slot()
+        .HAlign(HAlign_Right)
+        .Padding(10.0f)
+        [
+            SNew(SButton)
+            .Text(LOCTEXT("BakeButton", "Bake Material"))
+        ];
 
-
-	// 作成したウィジェットをDockTabのコンテンツとして設定
-	return SNew(SDockTab)
-		.TabRole(ETabRole::NomadTab)
-		[
-			WidgetContent
-		];
+    return SNew(SDockTab)
+        .TabRole(ETabRole::NomadTab)
+        [
+            WidgetContent
+        ];
 }
 
 void FMaterialBakerModule::PluginButtonClicked()
